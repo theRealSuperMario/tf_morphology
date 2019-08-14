3f8ca457f993fa1724f98a2dc4641ac17e79e2a8
supermario94123 <supermario94123@gmail.com>
2019-08-14 08:12:48 +0200

    added test for grayscale morphology

--- test_pytest.py
+++ test_pytest.py
@@ -1,14 +1,37 @@
+import numpy as np
 import pytest
 import skimage
 import tensorflow as tf
 from skimage import data, io
 from skimage.util import img_as_ubyte
+
 import tf_morphology as tfm
-import numpy as np
 
 tf.enable_eager_execution()
 
-# TODO: test against scikit-image functionality
+
+def setup_grayscale_test():
+    coins = data.coins()
+    tf_coins = tf.reshape(coins, (1, coins.shape[0], coins.shape[1], 1))
+    tf_coins = tf.to_int32(tf_coins)
+    selem = skimage.morphology.disk(6)
+    tf_selem = tf.cast(selem, tf.int32)
+    tf_selem = tf.reshape(tf_selem, (13, 13, 1))
+    return (coins, tf_coins), (selem, tf_selem)
+
+
+equivalent_grayscale_functions = [
+    (tfm.opening2d, skimage.morphology.opening),
+    (tfm.closing2d, skimage.morphology.closing),
+]
+
+
+@pytest.mark.parametrize("tf_func, sk_func", equivalent_grayscale_functions)
+def test_grayscale_operations(tf_func, sk_func):
+    (mask, tf_mask), (selem, tf_selem) = setup_grayscale_test()
+    tf_processed = tf_func(tf_mask, tf_selem)
+    processed = sk_func(mask, selem)
+    assert np.allclose(processed.astype(np.int32), np.squeeze(tf_processed))
 
 
 def setup_binary_test():
@@ -20,17 +43,6 @@ def setup_binary_test():
     tf_selem = tf.cast(selem, tf.int32)
     tf_selem = tf.reshape(tf_selem, (13, 13, 1))
     return (horse, tf_horse), (selem, tf_selem)
-
-
-def setup_grayscale_test():
-    # TODO:
-    # return (coins, tf_coins), (selem, tf_selem)
-    pass
-
-
-def test_grayscale_operations(tf_func, sk_func):
-    # TODO
-    pass
 
 
 equivalent_binary_functions = [
@@ -47,3 +59,14 @@ def test_binary_operations(tf_func, sk_func):
     tf_processed = tf_func(tf_mask, tf_selem)
     processed = sk_func(mask, selem)
     assert np.allclose(processed.astype(np.int32), np.squeeze(tf_processed))
+
+
+class Test_structuringElements(object):
+    def setup(self,):
+        coins = data.coins()
+        n_channels = 3
+        coins = np.stack([coins] * n_channels, axis=-1)
+        tf_coins = tf.reshape(tf_coins, (1, 328, 400, n_channels))
+        selem = skimage.morphology.disk(6)
+        tf_selem = tfm.disk(6, n_channels)
+        return (coins, tf_coins), (selem, tf_selem)
